527cdde4c7e1fb578936e821539588247dab1824
polad <poladgm@gmail.com>
2017-09-24 11:34:07 +0300

    core spring concepts

---
+++ 14-spring_bean_postprocessor/tutorial/spring/beans/Triangle.java
@@ -0,0 +1,35 @@
+package tutorial.spring.beans;
+
+public class Triangle {
+
+	Point a;
+    Point b;
+    Point c;
+	public Point getA() {
+		return a;
+	}
+	public void setA(Point a) {
+		this.a = a;
+	}
+	public Point getB() {
+		return b;
+	}
+	public void setB(Point b) {
+		this.b = b;
+	}
+	public Point getC() {
+		return c;
+	}
+	public void setC(Point c) {
+		this.c = c;
+	}
+    
+    public void draw(){
+    	System.out.println("point A cordinates "+a.getX()+" : "+a.getY());
+
+    	System.out.println("point B cordinates "+b.getX()+" : "+b.getY());
+
+    	System.out.println("point C cordinates "+c.getX()+" : "+c.getY());
+    }
+	
+}
