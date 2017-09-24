ccfc3ec4bba3c3b2cb9f8b79dc01cda7fa84a7a1
polad <poladgm@gmail.com>
2017-09-24 22:38:09 +0300

    component and stereotype annotations

---
+++ 20-spring_component_and_stereotype_annotations/tutorial/spring/beans/Circle.java
@@ -0,0 +1,42 @@
+package tutorial.spring.beans;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+
+// @Service
+//@Controller
+
+@Component
+public class Circle {
+	Point center;
+
+	public Point getCenter() {
+		return center;
+	}
+
+	
+	@Resource(name="pointA")
+	public void setCenter(Point center) {
+		this.center = center;
+	}
+	@PostConstruct
+	public void initializeCircle(){
+		System.out.println("circle initialization");
+	}
+	
+	@PreDestroy
+	public void destroyCircle(){
+		System.out.println("circle destroyed");
+	}
+
+	public void draw() {
+		System.out.println("point A cordinates " + center.getX() + " : " + center.getY());
+
+		
+	}
+
+	
+}
