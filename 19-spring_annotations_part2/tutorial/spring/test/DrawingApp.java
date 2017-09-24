2b4273a9dc4093fe4640486f72e01176e279370e
polad <poladgm@gmail.com>
2017-09-24 19:46:01 +0300

    resource , Postconstruct ,PreDestroy annotations are implemented

---
+++ 19-spring_annotations_part2/tutorial/spring/test/DrawingApp.java
@@ -0,0 +1,22 @@
+package tutorial.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.expression.spel.ast.AstUtils;
+
+import tutorial.spring.beans.Circle;
+import tutorial.spring.beans.Triangle;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+				"spring.xml");
+		context.registerShutdownHook();
+		Circle circle = (Circle) context.getBean("circle");
+		circle.draw();
+	}
+
+}
